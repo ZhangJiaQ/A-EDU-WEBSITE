6d7581007beeeecfcc0096d2443475b9231ff99d
ZhangJiaQ <529326573@qq.com>
2017-08-15 22:51:44 +0800

    0.3

--- apps/course/models.py
+++ apps/course/models.py
@@ -21,6 +21,8 @@ class Course(models.Model):
         verbose_name = '课程'
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return self.name
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, verbose_name='课程')
@@ -30,6 +32,9 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = '章节'
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 
 class Video(models.Model):
--- apps/users/models.py
+++ apps/users/models.py
@@ -18,20 +18,20 @@ class UserProfile(AbstractUser):
         verbose_name = '用户信息'
         verbose_name_plural = verbose_name
 
-    def __unicode__(self):
+    def __str__(self):
         return self.username
 
 
 class EmailVerifyRecord(models.Model):
     code = models.CharField(max_length=20, verbose_name='验证码')
     email =models.EmailField(max_length=50, verbose_name='邮箱')
-    send_type = models.CharField(choices=(('register','zhuce'),('forget','wangjimima')), verbose_name='验证码类型', max_length=12)
+    send_type = models.CharField(choices=(('register','注册验证码'),('forget','找回验证码')), verbose_name='验证码类型', max_length=12)
     send_time = models.DateTimeField(default=datetime.now, verbose_name='发送时间')
-    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
 
     class Meta:
         verbose_name = '邮箱验证码'
         verbose_name_plural = verbose_name
+
 
 class PageBanner(models.Model):
     title = models.CharField(max_length=50, verbose_name='标题')
@@ -41,5 +41,5 @@ class PageBanner(models.Model):
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加图片时间')
 
     class Meta:
-        verbose_name = 'lunbotu'
+        verbose_name = '轮播图'
         verbose_name_plural = verbose_name
