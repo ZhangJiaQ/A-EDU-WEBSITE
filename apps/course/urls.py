f6c619fbada9dcadcf2b706ec45dd49f0352acad
ZhangJiaQ <529326573@qq.com>
2017-09-15 23:19:40 +0800

    0.25

--- apps/course/urls.py
+++ apps/course/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from apps.course.views import CourseView
+from apps.course.views import CourseView, CourseDetail
 
 urlpatterns = [
     url(r'^list/$', CourseView.as_view(), name='org_list'),
+    url(r'^detail/(?P<course_id>\d+)/$', CourseDetail.as_view(), name='org_detail'),
 
 ]
--- apps/course/views.py
+++ apps/course/views.py
@@ -8,7 +8,15 @@ from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 class CourseView(View):
     def get(self, request):
-        all_courses = Course.objects.all()
+        all_courses = Course.objects.all().order_by('-add_time')
+
+        hot_courses = Course.objects.order_by('-click_nums')[:3]
+
+        sort = request.GET.get('sort', '')
+        if sort == 'hot':
+            all_courses = all_courses.order_by('-click_nums')
+        elif sort == 'student':
+            all_courses = all_courses.order_by('-students')
 
         # 分页功能实现
         try:
@@ -21,5 +29,12 @@ class CourseView(View):
 
         #返回页面至前端
         return render(request, 'course-list.html', {
-            'all_courses':courses
+            'all_courses':courses,
+            'sort':sort,
+            'hot_courses':hot_courses,
+        })
+
+class CourseDetail(View):
+    def get(self, request, course_id):
+        return render(request, 'course-detail.html', {
         })
